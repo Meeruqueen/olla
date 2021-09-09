52bedf1971e3843377358f8c3e2afc030d7c9f6d
Meeruqueen <90372782+Meeruqueen@users.noreply.github.com>
2021-09-09 10:56:53 +0530

    Add files via upload

---
+++ ollaproject.py
@@ -0,0 +1,50 @@
+# print("wellcome to olla app")
+# corrent_location=input("please enter your corrent location")
+# destination=input("enter your destination location")
+# nearby_riders_availbal=[["surajl","cab"],["rahul","cab"],["ram","outo"],["monu","outo"],["golu","cab"]]
+# details_of_riders=[[7073172288,"123xyz","black","sujuki"],[707317228,"1234xyz","white","honda"],[7878371483,"394fue","blue","sujuki"],[7073172288,"4058fht","skyblue","honda"],[7878371483,"123ms","black","shiv"]]
+# price=[[500],[400],[400],[800]]
+# i=0
+# for i in range(len("nearby_drivers_availbal")):
+#     print("nearby_drivers_availb")
+# driver1="suraj"
+# driver2="rahul"
+# driver3="ram"
+# driver4="monu"
+# driver5="golu"
+# riders=input("enter the driver you would like to choose")
+# if riders=="suraj":
+#     print('details_of_driver',price,"cab will be coming in 10 mints")
+# elif riders=="rahul":
+#     print('details_of_driver',price,"cab will be coming in 15 mints")
+# elif riders=="ram":
+#     print('datails_of_driver',price,"outo will be coming in 12 mints")
+# elif riders=="monu":
+#     print('details_of_driver',price,"outo will be coming in 23 mints")
+# elif riders=="golu":
+#     print('details_of__driver',price,"cab will be coming in 17 mints")
+# else:
+#     print("no service availbal")
+# otp=int(input("enter the otp"))
+# if otp==1212:
+#     print("correct")
+# else:
+#     print("not correct")
+# payment=input("enter the payment")
+# if payment=="case":
+#     print("given case")
+# elif payment=="upi payment":
+#     print("scan the your code")
+# elif payment=="google pay":
+#     print("scan the your QR code")
+
+# feedback=input("enter the your feedback")
+# if feedback=="*****":
+#     print("exelent")
+# elif feedback=="****":
+#     print("very good")
+# elif feedback=="***":
+#     print("good")
+# elif feedback=="***":
+#     print("bad")
+# print("thanku")
